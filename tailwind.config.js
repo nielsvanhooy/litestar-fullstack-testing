5023be9ad98bf3a612fdbcbdfbc388142512df8b
Nvhooij <Sjaakie123!>
2023-09-11 13:36:56 +0200

    changed vue to react. with deps i need to get started

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/app/domain/web/resources/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
