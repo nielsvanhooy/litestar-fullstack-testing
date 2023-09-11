dbb391f0434203e9fdda44efb8d6030ea0216793
niels van hooij <niels.vanhooij@kpn.com>
2023-09-11 10:27:59 +0200

    chars

--- src/app/domain/ssh_terminal/controllers/ssh_terminal_controller.py
+++ src/app/domain/ssh_terminal/controllers/ssh_terminal_controller.py
@@ -42,9 +42,9 @@ class SshWebTerminalController(Controller):
     async def ssh_web_terminal(self, data: str, socket: WebSocket) -> str:
         # """ this is "a testing method, not for use now. but exploring options on what i want ""
 
-        async with asyncssh.connect('192.168.1.35', username="donny", password="sjaakie123", known_hosts=None, connect_timeout=10) as conn:
+        async with asyncssh.connect('10.1.1.142', username="lagen008", password="lagen008", known_hosts=None, connect_timeout=10) as conn:
             output = StringIO()
-            (sshWriter, sshReader, sshExReader)= await conn.open_session(term_type='xterm-256color') # vt100 for cisco and huawi?
+            (sshWriter, sshReader, sshExReader)= await conn.open_session(term_type='vt100') # vt100 for cisco and huawi?
             ssh_task = asyncio.create_task(sshReader.read(1024))
             sshex_task = asyncio.create_task(sshExReader.read(1024))
             ws_task = asyncio.create_task(socket.receive_data(mode="text"))
@@ -55,7 +55,10 @@ class SshWebTerminalController(Controller):
 
                 if ws_task in done:
                     msg = ws_task.result()
-                    msg += "\r"
+                    if "x03" in msg:
+                        msg = "\x03"
+                    else:
+                        msg += "\r"
                     sshWriter.writelines([msg])
                     await sshWriter.drain()
                     ws_task = asyncio.create_task(socket.receive_data(mode="text"))
